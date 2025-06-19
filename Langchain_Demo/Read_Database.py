3104f32c20c8bead0789393d98d0cc35f5f72afb
Yiwei <hehegg321@Gmail.com>
2025-06-19 10:17:38 -0400

    RAG demo

---
+++ Langchain_Demo/Read_Database.py
@@ -0,0 +1,64 @@
+#  Using chain and agents to achieve data retrieval from SQL and provide natraul language
+# 1. use model to change user input to SQL inspection
+# 2, execute SQL search
+# 3. Use model to respond to user
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.sql_database.query import create_sql_query_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.tools import QuerySQLDatabaseTool
+from langchain_core.runnables import RunnableWithMessageHistory, RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.retrieval import create_retrieval_chain
+import bs4
+from operator import itemgetter
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage, SystemMessage
+import os
+from langchain_core.vectorstores import VectorStore
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langgraph.prebuilt import chat_agent_executor
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.utilities import SQLDatabase
+
+
+
+os.environ["LANGCHAIN_TRACING_V2"] = "True"
+os.environ["LANGCHAIN_PROJECT"] = "LangchainDemo"
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash-preview-05-20')
+
+#sqlalchemy
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'sakila'
+USERNAME = 'root'
+PASSWORD = '12345'
+
+MYSQL_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+
+db= SQLDatabase.from_uri(MYSQL_URI)
+
+print(db.get_usable_table_names())
+
+# directly pull answers from SQL using LLM Model
+test_chain = create_sql_query_chain(model,db)
+response = test_chain.invoke({'question': 'how many rows are there in the actor table?'})
+print(response)
+
+answer_prompt = ChatPromptTemplate.from_template(
+    """given the following SQL database, SQL command and the user question, provide the answer to the question:
+    Question: {question}
+    SQL Query:{query}
+    SQL Result:{result}
+    Answer:  
+    """
+)
+# establish the tool to execute sql command
+execute_sql_tool = QuerySQLDatabaseTool(db)
+
+chain = (RunnablePassthrough.assign(query = test_chain).assign(result=itemgetter('query')))
